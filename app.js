e3a218b310b6eb1927cf9c98ab6416dbb713430e
ThePardeep <pardeepsaini7927@gmail.com>
2020-05-01 19:37:00 +0530

    fc

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const database = require("./Helper/Database.js");
 const bodyParser = require("body-parser");
 var schedule = require("node-schedule");
 const path = require("path");
-var rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = 6;
-rule.hour = 23;
-rule.minute = 0;
-rule.second = 0;
 
 const app = express();
 
@@ -39,23 +34,17 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//SET STATIC FOLDER
-app.use("/static", express.static("/public"));
-app.use(express.static("client/build"));
-
 //PRODUCTION Setup
 
 if (process.env.NODE_ENV === "production") {
+  // SET STATIC FOLDER
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client/build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-var j = schedule.scheduleJob(rule, function () {
-  console.log("The answer to life, the universe, and everything!");
-});
 //Routes
 app.use("/covid", CovidRoute);
 

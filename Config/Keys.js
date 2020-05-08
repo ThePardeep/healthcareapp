0c4ded4ef93c1b8622a3ce3baa1dc4c85a3a966f
ThePardeep <pardeepsaini7927@gmail.com>
2020-05-08 19:33:58 +0530

    fc

--- Config/Keys.js
+++ Config/Keys.js
@@ -1,4 +1,9 @@
 module.exports = {
-  JwtSecret: process.env.JwtSecret,
-  mapkey: process.env.MAP_KEY,
+  JwtSecret: "hkapp",
+  mapkey: "AIzaSyDr-WOPersRg2vNlU4xiTT050ewDnwgNiA",
 };
+
+// module.exports = {
+//   JwtSecret: process.env.JwtSecret,
+//   MAP_KEY: process.env.MAP_KEY,
+// };
